3c3c2a27a4ee7b4c668583a4f4fd44acd20d7d17
Chuck Rakaczky <cjr105@case.edu>
2024-01-20 12:38:48 -0500

    created compensation context, repo, interface, and model

---
+++ dotnet-code-challenge-July2022/dotnet-code-challenge/CodeChallenge/Repositories/CompensationRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeChallenge.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CodeChallenge.Data;
+
+namespace CodeChallenge.Repositories
+{
+    public class CompensationRepository : ICompensationRepository
+    {
+        private readonly CompensationContext _compContext;
+        private readonly ILogger<ICompensationRepository> _logger;
+
+        public CompensationRepository(ILogger<ICompensationRepository> logger, CompensationContext compContext)
+        {
+            _compContext = compContext;
+            _logger = logger;
+        }
+
+        public Compensation Add(Compensation compensation)
+        {
+            _compContext.Compensations.Add(compensation);
+            return compensation;
+        }
+
+        public Compensation GetById(string id)
+        {
+            var compensations = _compContext.Compensations;
+            return compensations.SingleOrDefault(c => c.Employee.EmployeeId == id);
+        }
+
+        public Task SaveAsync()
+        {
+            return _compContext.SaveChangesAsync();
+        }
+    }
+}
